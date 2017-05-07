caf75ef8fcf5fece555a5243811f94cdd4221bf8
Ryan Armstrong <ryan@cavaliercoder.com>
2017-05-07 22:11:10 +0800

    Improved filename tests

--- filename_test.go
+++ filename_test.go
@@ -4,22 +4,48 @@ import "testing"
 import "net/http"
 
 func TestURLFilenames(t *testing.T) {
-	tests := map[string]string{
-		"http://test.com/path/filename": "filename",
+	expect := "filename"
+
+	shouldPass := []string{
+		"http://test.com/filename",
+		"http://test.com/path/filename",
+		"http://test.com/deep/path/filename",
+		"http://test.com/filename?with=args",
+		"http://test.com/filename#with-fragment",
 	}
 
-	for url, expect := range tests {
+	shoudlFail := []string{
+		"http://test.com",
+		"http://test.com/",
+		"http://test.com/filename/",
+		"http://test.com/filename/?with=args",
+		"http://test.com/filename/#with-fragment",
+	}
+
+	for _, url := range shouldPass {
 		req, _ := http.NewRequest("GET", url, nil)
 		resp := &http.Response{
 			Request: req,
 		}
 		actual, err := guessFilename(resp)
 		if err != nil {
-			panic(err)
+			t.Errorf("%v", err)
 		}
 
 		if actual != expect {
 			t.Errorf("expected '%v', got '%v'", expect, actual)
+		}
+	}
+
+	for _, url := range shoudlFail {
+		req, _ := http.NewRequest("GET", url, nil)
+		resp := &http.Response{
+			Request: req,
+		}
+
+		_, err := guessFilename(resp)
+		if err != ErrNoFilename {
+			t.Errorf("expected '%v', got '%v'", ErrNoFilename, err)
 		}
 	}
 }
