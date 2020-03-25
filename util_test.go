3241eec245eab0633af57bfc4da604a336af3e9f
Olivier C <git@olivier.pfad.fr>
2020-03-25 16:00:14 +0100

    better test

--- util_test.go
+++ util_test.go
@@ -49,9 +49,11 @@ func TestURLFilenames(t *testing.T) {
 			t.Run(tc, func(t *testing.T) {
 				req, err := http.NewRequest("GET", tc, nil)
 				if err != nil {
-					// Since go1.12, urls with invalid control character return an error
-					// See https://github.com/golang/go/commit/829c5df58694b3345cb5ea41206783c8ccf5c3ca
-					t.Skip()
+					if tc == "http://test.com/filename\x00" {
+						// Since go1.12, urls with invalid control character return an error
+						// See https://github.com/golang/go/commit/829c5df58694b3345cb5ea41206783c8ccf5c3ca
+						t.Skip()
+					}
 				}
 				resp := &http.Response{
 					Request: req,
